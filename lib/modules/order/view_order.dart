e62705e8c931afa34be698bf52a6c90f3837f98c
Roshan Shrestha <85292792+roshanxshrestha@users.noreply.github.com>
2023-10-05 20:43:08 +0545

    part 6 complete

--- lib/modules/order/view_order.dart
+++ lib/modules/order/view_order.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:food_delivery/controllers/order_controller.dart';
 import 'package:food_delivery/models/order_model.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimension.dart';
 import 'package:get/get.dart';
 
 class ViewOrder extends StatelessWidget {
@@ -22,7 +24,71 @@ class ViewOrder extends StatelessWidget {
                   ? orderController.currentOrderList.reversed.toList()
                   : orderController.historyOrderList.reversed.toList();
             }
-            return Text(orderList.length.toString());
+            return SizedBox(
+              width: Dimension.screenWidth,
+              child: Padding(
+                padding: EdgeInsets.all(Dimension.width10 / 2),
+                child: ListView.builder(
+                    itemCount: orderList.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                    'Order No.: ${orderList[index].id.toString()}'),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(
+                                          Dimension.height10 / 2),
+                                      decoration: BoxDecoration(
+                                        color: AppColors.mainColor,
+                                        borderRadius: BorderRadius.circular(
+                                          Dimension.radius20 / 4,
+                                        ),
+                                      ),
+                                      child: Text(
+                                        '${orderList[index].orderStatus}',
+                                        style: const TextStyle(
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                    SizedBox(height: Dimension.height10 / 2),
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        padding: EdgeInsets.all(
+                                            Dimension.height10 / 2),
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.circular(
+                                            Dimension.radius20 / 4,
+                                          ),
+                                          border: Border.all(
+                                            width: 1,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ),
+                                        ),
+                                        child: Text("Track Order"),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            SizedBox(height: Dimension.height10),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+            );
           } else {
             return Text("Loading");
           }
